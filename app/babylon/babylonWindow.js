566a328839d867771513cea020f7092eb25b8897
Mateusz Kulewicz <mateusz.kulewicz@gmail.com>
2016-01-23 23:37:21 +0100

    Integrate babylon.js with angular as a directive

---
+++ app/babylon/babylonWindow.js
@@ -0,0 +1,31 @@
+var babylonWindow = angular.module("BabylonWindow", []);
+
+babylonWindow.directive("babylon",
+	[function() {
+		return {
+			restrict: "A",
+			link: function (scope, elem, attr) {
+				canvas = elem[0];
+				var engine = new BABYLON.Engine(canvas, true);
+				var createScene = function() {
+					var scene = new BABYLON.Scene(engine);
+					var camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5,-10), scene);
+					camera.setTarget(BABYLON.Vector3.Zero());
+					camera.attachControl(canvas, false);
+					var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0,1,0), scene);
+					var sphere = BABYLON.Mesh.CreateSphere('sphere1', 16, 2, scene);
+					sphere.position.y = 1;
+					var ground = BABYLON.Mesh.CreateGround('ground1', 6, 6, 2, scene);
+					return scene;
+				}
+				var scene = createScene();
+				engine.runRenderLoop(function() {
+					scene.render();
+				});	
+				window.addEventListener('resize', function() {
+					engine.resize();
+				});
+			}
+		}
+	}]
+);
---
+++ app/index.js
@@ -0,0 +1,4 @@
+"use strict";
+
+var choochooApp = angular.module('choochooApp', ['BabylonWindow']);
+
